70b5a6dfb3413c46004d9b778b99b3358999570c
Eunicegeorge <georgeunice408@gmail.com>
2023-06-26 11:15:31 -0700

    task 0

---
+++ test/_printf.c
@@ -0,0 +1,44 @@
+#include "main.h"
+
+int _printf(const char *format, ...)
+{
+	unsigned int i, n_val, s_val;
+
+	va_list args;
+
+	va_start(args, format);
+
+	for (i = 0; format[i] != '\0'; i++)
+	{
+		if (format[i] != '%')
+		{
+			puchar(format[i]);
+			n_val += 1;
+		}
+		else
+		{
+			i++;
+			switch (format[i])
+			{
+				case 'c':
+					puchar(va_arg(args, int));
+					n_val++;
+					break;
+				case 's':
+					s_val = put_s(va_arg(args, char *));
+					n_val += s_val;
+					break;
+				case '%':
+					puchar('%');
+					n_val++;
+					break;
+				default:
+					puchar(format[i]);
+					n_val++;
+					break;
+			}
+		}
+	}
+	va_end(args);
+	return (n_val);
+}
---
+++ test/main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdarg.h>
+#include <unistd.h>
+
+int put_s(char *string);
+int puchar(char c);
+int _printf(const char *format, ...);
+#endif
